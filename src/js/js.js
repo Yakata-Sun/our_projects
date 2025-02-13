32e86ba27c89f40ce726003e18e0dcf0099de5c6
Yakata <vajra.de@gmail.com>
2025-02-13 14:25:16 +0300

    add modal

--- src/js/js.js
+++ src/js/js.js
@@ -7,6 +7,15 @@ $(document).ready(function(){
     adaptiveHeight: true,
     autoplaySpeed: 2000,
     prevArrow: '<button type="button" class="slick-prev"><img src="icons/arrowLeft.ico"></button>',
-    nextArrow: '<button type="button" class="slick-next"><img src="icons/arrowRight.ico"></button>'
+    nextArrow: '<button type="button" class="slick-next"><img src="icons/arrowRight.ico"></button>',
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+        dots: true,
+        arrows: false
+        }
+      }
+    ]
   });
 });
